de6bcce7e5da00c822cdaa8743f08a12b7905cd6
Chaithanya Seshamsetti <chaitanyaseshamsetty@gmail.com>
2026-07-28 14:16:37 +0530

    Add functions for printing positive and negative numbers
    Implement functions to print positive and negative integers.

--- Day02/README.md /solutions/05_zero_converter.py
+++ Day02/README.md /solutions/05_zero_converter.py
@@ -1,1 +1,12 @@
-
+def pos(n):
+    ## Write the code
+    n = n-1
+    while n >= 0:
+        print (n, end =" ")
+        n-=1
+    
+def neg(n):
+    ##Write the code
+    while n <= 0:
+        print(n , end=" ")
+        n+=1
