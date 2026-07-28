fd9b5cbea20eb57c2a4dfdfd345d46952fd5d17b
Chaithanya Seshamsetti <chaitanyaseshamsetty@gmail.com>
2026-07-28 14:13:42 +0530

    Update 11_calculator.py

--- Day02/README.md /solutions/11_calculator.py
+++ Day02/README.md /solutions/11_calculator.py
@@ -1,1 +1,14 @@
+class Solution:
+    def calculate(self, a: int, b: int, operator: int) -> None:
 
+        if operator == 1:
+            print(a + b, end="")
+
+        elif operator == 2:
+            print(a - b, end="")
+
+        elif operator == 3:
+            print(a * b, end="")
+
+        else:
+            print("Invalid Input", end="")
