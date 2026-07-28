3aa5fa16b0d922db2eb3a1fc076c8d8a8d6d25a5
Chaithanya Seshamsetti <chaitanyaseshamsetty@gmail.com>
2026-07-28 14:14:09 +0530

    Update 10_leap_year.py

--- Day02/README.md /solutions/10_leap_year.py
+++ Day02/README.md /solutions/10_leap_year.py
@@ -1,1 +1,12 @@
-
+class Solution:
+    def checkYear (self, n):
+        # code here
+        if n % 400 == 0:
+           return True
+        elif n % 100 == 0 and n % 400 != 0 :
+            return False
+        elif n % 4 == 0 and n % 100 != 0:
+            return True
+        else :
+            return False
+        
