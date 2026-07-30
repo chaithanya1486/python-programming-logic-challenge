cb586a951f8deccc6ba6c753a1ec030714f2068c
Chaithanya Seshamsetti <chaitanyaseshamsetty@gmail.com>
2026-07-30 11:41:30 +0530

    Create 04_reverse_digits.py

---
+++ Day03/problems_solved/04_reverse_digits.py
@@ -0,0 +1,11 @@
+class Solution:
+    def reverseDigits(self, n):
+
+        rev = 0
+
+        while n > 0:
+            digit = n % 10
+            rev = rev * 10 + digit
+            n //= 10
+
+        return rev
