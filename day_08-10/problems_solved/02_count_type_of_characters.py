86f4d4f9b04230b88b6498c7b01a122cd9e154fb
Chaithanya Seshamsetti <chaitanyaseshamsetty@gmail.com>
2026-08-08 23:03:00 +0530

    Create 02_count_type_of_characters.py

---
+++ day_08-10/problems_solved/02_count_type_of_characters.py
@@ -0,0 +1,18 @@
+class Solution:
+    def count(self, s):
+        upper = 0
+        lower = 0
+        digit = 0
+        special = 0
+
+        for ch in s:
+            if ch.isupper():
+                upper += 1
+            elif ch.islower():
+                lower += 1
+            elif ch.isdigit():
+                digit += 1
+            else:
+                special += 1
+
+        return [upper, lower, digit, special]
